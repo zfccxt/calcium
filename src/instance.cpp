20c0f29153610f700942fece42c5770a488cc745
Milgate J (FCES) <18029949@students.southwales.ac.uk>
2022-01-20 14:16:59 +0000

    CMake tests for Vulkan availability

--- src/instance.cpp
+++ src/instance.cpp
@@ -1,17 +1,20 @@
 #include "instance.hpp"
 
 #include "opengl/opengl_instance.hpp"
-#include "vulkan/vulkan_instance.hpp"
+#ifdef CALCIUM_VULKAN_SDK_FOUND
+  #include "vulkan/vulkan_instance.hpp"
+#endif
 
 namespace cl {
 
 std::unique_ptr<Instance> CreateInstance(Backend backend) {
-  if (backend == Backend::kOpenGL) {
-    return std::make_unique<OpenGL::OpenGLInstance>();
-  }
-  else {
+#ifdef CALCIUM_VULKAN_SDK_FOUND
+  if (backend == Backend::kVulkan) {
     return std::make_unique<Vulkan::VulkanInstance>();
   }
+#endif
+
+  return std::make_unique<OpenGL::OpenGLInstance>();
 }
 
 }
