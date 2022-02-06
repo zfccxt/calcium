b277a859c8515336f7bb215b1e905d0b50d74761
Jack <zfccxt@gmail.com>
2022-02-06 12:41:01 +0000

    ShaderDataType SizeOf function

--- src/shader_data_type.hpp
+++ src/shader_data_type.hpp
@@ -1,13 +1,16 @@
 #pragma once
 
+#include <cstddef>
+
 namespace cl {
 
 enum class ShaderDataType : char {
   kBool,
   kInt,     kInt2,   kInt3,   kInt4,
   kFloat,   kFloat2, kFloat3, kFloat4,
-  kMatrix3, kMatrix4,
-  kUndefined
+  kMatrix3, kMatrix4
 };
+
+size_t SizeOf(ShaderDataType type);
 
 }
