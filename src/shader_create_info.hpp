78dc8d887e5981018eb263b3d7abf0c337838700
Jack <zfccxt@gmail.com>
2022-02-06 15:37:12 +0000

    Include file paths for other shader types in ShaderCreateInfo

--- src/shader_create_info.hpp
+++ src/shader_create_info.hpp
@@ -7,6 +7,11 @@ namespace cl {
 struct ShaderCreateInfo {
   std::string vert_path;
   std::string frag_path;
+  std::string geom_path;
+  std::string mesh_path;
+  std::string task_path;
+  std::string tess_path;
+  std::string comp_path;
 };
 
 }
