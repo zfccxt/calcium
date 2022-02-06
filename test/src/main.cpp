b7b84bb7a3fb9d631602e77b5df098cb2a7f4628
Jack <zfccxt@gmail.com>
2022-02-06 12:46:27 +0000

    Test code in main.cpp

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -3,17 +3,22 @@
 int main() {
   auto context = cl::CreateContext(cl::Backend::kOpenGL);
 
-  auto window = context->CreateWindow();
+  cl::WindowCreateInfo window_info;
+  window_info.clear_colour = 0x336699ff;
+  window_info.enable_depth_test = false;
+  auto window = context->CreateWindow(window_info);
 
   cl::ShaderCreateInfo shader_info;
   shader_info.vert_path = "res/shaders/shader.vert.glsl";
   shader_info.vert_path = "res/shaders/shader.frag.glsl";
   auto shader = context->CreateShader(shader_info);
 
+
   while (window->IsOpen()) {
     cl::PollWindowEvents();
 
     context->BindRendertarget(window);
+    window->Clear();
     window->SwapBuffers();
   }
 }
