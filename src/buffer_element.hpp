7e6f047355011e1e6403f3a1ed4b32642525c153
Jack <zfccxt@gmail.com>
2022-02-06 12:47:20 +0000

    BufferElement struct

---
+++ src/buffer_element.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <cstddef>
+
+#include "shader_data_type.hpp"
+
+namespace cl {
+
+// A wrapper around a ShaderDataType, caching size and offset
+// To be used by BufferLayout
+struct BufferElement {
+  BufferElement(ShaderDataType type) : type(type), size(SizeOf(type)), offset(0) { }
+
+  ShaderDataType type;
+  size_t size;
+  size_t offset;
+};
+
+}
